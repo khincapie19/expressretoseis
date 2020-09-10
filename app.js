5169b8a93083b13506f74868029096bb2e9f8a7a
Katherine <katherinehincapie19@hotmail.com>
2020-09-10 19:30:12 -0400

    Primer Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "pug");// configuraciòn para unir con pug
+app.set("views", "views"); //  decimos donde queremos guardar esos archivos
+app.use(express.urlencoded({extended: true}));
+app.use("/static", express.static("public"));
+
+app.get('/', (req, res) => {
+  res.render("new");
+});
+
+app.post('/', (req, res) => {
+  res.send(`Hola ${req.body.name}!`);
+  res.redirect("/");
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
