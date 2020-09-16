f0fb318df42472b147fb9cd624db90666e18c27c
Katherine <katherinehincapie19@hotmail.com>
2020-09-16 18:37:55 -0400

    cambios en las dependencias

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var app = express();
 app.set("view engine", "pug");// configuraciòn para unir con pug
 app.set("views", "views"); //  decimos donde queremos guardar esos archivos
 app.use(express.urlencoded({extended: true}));
-app.use("/static", express.static("public"));
 
 app.get('/', (req, res) => {
   res.render("new");
@@ -12,7 +11,6 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   res.send(`Hola ${req.body.name}!`);
-  res.redirect("/");
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
